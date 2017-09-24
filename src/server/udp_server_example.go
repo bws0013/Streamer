657adb54cffd3c50abf34f8dcfeb28c196827482
Ben <i@auburn.edu>
2017-09-23 21:40:36 -0500

    Figure out the sending between server/client such that server broadcasts after client has connected.

--- src/server/udp_server_example.go
+++ src/server/udp_server_example.go
@@ -4,6 +4,8 @@ import (
   "fmt"
   "log"
   "net"
+  "time"
+  "strconv"
 )
 
 func main() {
@@ -27,12 +29,50 @@ func server() error {
 
   buf := make([]byte, 1024)
 
-  for {
-    n,addr,err := ServerConn.ReadFromUDP(buf)
-    fmt.Println("Received ",string(buf[0:n]), " from ",addr)
+  n,addr,err := ServerConn.ReadFromUDP(buf)
 
+  fmt.Println(n, "->", addr)
+
+  if err != nil { log.Fatal(err) }
+
+  address := string(addr.IP) + ":" + string(addr.Port)
+
+
+  client(address)
+
+
+  return nil
+  // for {
+  //
+  //   fmt.Println("Received ",string(buf[0:n]), " from ",addr)
+  //   ServerConn.Write([]byte("0"))
+  //   if err != nil {
+  //       fmt.Println("Error: ",err)
+  //   }
+  // }
+}
+
+func client(addr string) {
+  ServerAddr,err := net.ResolveUDPAddr("udp",addr)
+  if err != nil { log.Fatal(err) }
+
+  LocalAddr, err := net.ResolveUDPAddr("udp", "127.0.0.1:0")
+  if err != nil { log.Fatal(err) }
+
+  Conn, err := net.DialUDP("udp", LocalAddr, ServerAddr)
+  if err != nil { log.Fatal(err) }
+
+  defer Conn.Close()
+  i := 0
+  for {
+    msg := strconv.Itoa(i)
+    i++
+    buf := []byte(msg)
+    _,err := Conn.Write(buf)
     if err != nil {
-        fmt.Println("Error: ",err)
+        fmt.Println(msg, err)
     }
+    time.Sleep(time.Second * 1)
   }
+
 }
